4f9f5e54974b720b917a8eed30617c4d407f2409
Noam Gal <noam.gal@codefresh.io>
2021-06-08 18:15:52 +0300

    fixed manifests

--- hack/build.sh
+++ hack/build.sh
@@ -13,6 +13,8 @@ go build -ldflags=" \
     -X 'github.com/codefresh-io/cli-v2/pkg/store.version=${VERSION}' \
     -X 'github.com/codefresh-io/cli-v2/pkg/store.buildDate=${BUILD_DATE}' \
     -X 'github.com/codefresh-io/cli-v2/pkg/store.gitCommit=${GIT_COMMIT}' \
-    -X 'github.com/codefresh-io/cli-v2/pkg/store.installationManifestsURL=${INSTALLATION_MANIFESTS_URL}' \
-    -X 'github.com/codefresh-io/cli-v2/pkg/store.installationManifestsNamespacedURL=${INSTALLATION_MANIFESTS_NAMESPACED_URL}'" \
+    -X 'github.com/codefresh-io/cli-v2/pkg/store.ArgoCDManifestsURL=${ARGOCD_INSTALLATION_MANIFESTS_URL}' \
+    -X 'github.com/codefresh-io/cli-v2/pkg/store.ArgoEventsManifestsURL=${EVENTS_INSTALLATION_MANIFESTS_URL}' \
+    -X 'github.com/codefresh-io/cli-v2/pkg/store.ArgoRolloutsManifestsURL=${ROLLOUTS_INSTALLATION_MANIFESTS_URL}' \
+    -X 'github.com/codefresh-io/cli-v2/pkg/store.ArgoWorkflowsManifestsURL=${WORKFLOWS_INSTALLATION_MANIFESTS_URL}' \
     -v -o ${OUT_FILE} ${MAIN}
