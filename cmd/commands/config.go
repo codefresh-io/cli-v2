89274aa206262bcc3beda30bbb0c3ae289e1cefe
roi.kramer <roi.kramer@codefresh.io>
2021-06-14 16:20:08 +0300

    fix

--- cmd/commands/config.go
+++ cmd/commands/config.go
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 // Copyright 2021 The Codefresh Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
->>>>>>> rebase
 package commands
 
 import (
