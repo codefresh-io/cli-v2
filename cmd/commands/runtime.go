8aa7ae7e8c98d3a1505f3155833991186f11c5b7
Noam Gal <noam.gal@codefresh.io>
2021-07-06 09:51:06 +0300

    fixed examples

--- cmd/commands/runtime.go
+++ cmd/commands/runtime.go
@@ -101,7 +101,7 @@ func NewRuntimeCreateCommand() *cobra.Command {
 
 # Adds a new runtime
 
-	<BIN> runtime create runtime-name --install-owner owner --install-name gitops_repo
+	<BIN> runtime create runtime-name --install-repo gitops_repo
 `),
 		PreRun: func(_ *cobra.Command, _ []string) {
 			if gsCloneOpts.Auth.Password == "" {
@@ -208,15 +208,15 @@ func NewRuntimeDeleteCommand() *cobra.Command {
 # To run this command you need to create a personal access token for your git provider
 # and provide it using:
 
-		export INSTALL_GIT_TOKEN=<token>
+		export GIT_TOKEN=<token>
 
 # or with the flag:
 
-		--install-git-token <token>
+		--git-token <token>
 
 # Adds a new runtime
 
-	<BIN> runtime delete runtime-name --install-owner owner --install-name gitops_repo
+	<BIN> runtime delete runtime-name --repo gitops_repo
 `),
 		PreRun: func(_ *cobra.Command, _ []string) {
 			cloneOpts.Parse()
