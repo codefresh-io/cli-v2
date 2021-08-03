340a6bd429138293e4264152d3abc37262faa69a
elad-codefresh <elad.laor@codefresh.io>
2021-08-03 18:09:51 +0300

    update

--- cmd/commands/runtime.go
+++ cmd/commands/runtime.go
@@ -278,13 +278,29 @@ func RunRuntimeList() error {
 
 	for _, rt := range runtimes {
 		status := "N/A"
-		namespace := rt.Metadata.Namespace
-		cluster := rt.Cluster
-		name := rt.Metadata.Name
-		version := rt.RuntimeVersion
+		namespace := "N/A"
+		cluster := "N/A"
+		name := "N/A"
+		version := "N/A"
 
 		if rt.HealthMessage != nil {
 			status = *rt.HealthMessage
+		}
+
+		if rt.Metadata.Namespace != "" {
+			namespace = rt.Metadata.Namespace
+		}
+
+		if rt.Cluster != "" {
+			cluster = rt.Cluster
+		}
+
+		if rt.Metadata.Name != "" {
+			name = rt.Metadata.Name
+		}
+
+		if rt.RuntimeVersion != "" {
+			version = rt.RuntimeVersion
 		}
 
 		_, err = fmt.Fprintf(tb, "%s\t%s\t%s\t%s\t%s\n",
