f3e236216b195e92b0a56f1e7b58f37c027af306
Noam Gal <noam.gal@codefresh.io>
2021-08-05 17:46:20 +0300

    added brew template

---
+++ brew/template.rb
@@ -0,0 +1,22 @@
+class CF2 < Formula
+  desc "Codefresh CLI tool, V2"
+  homepage "https://codefresh.io/"
+  url "https://github.com/codefresh-io/cli-v2.git",
+    tag:      "v{{ version }}",
+    revision: "{{ revision }}"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "cli-package", "DEV_MODE=false"
+    bin.install "dist/cf" => "cf"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cf version")
+
+    assert_match "must provide context name to use\"",
+      shell_output("#{bin}/cf config use-context 2>&1", 1)
+  end
+end
