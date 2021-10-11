a770c8c0bf9cec9143015d7d3cf1a535399dd2ae
rotem-cf <rotem.cohen@codefresh.io>
2021-10-11 17:18:40 +0300

    add repo to runtime.spec

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -56,6 +56,7 @@ type (
 		Components         []AppDef        `json:"components"`
 		Cluster            string          `json:"cluster"`
 		IngressHost        string          `json:"ingressHost"`
+		Repo               string          `json:"repo"`
 
 		devMode bool
 	}
