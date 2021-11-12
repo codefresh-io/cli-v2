9c1b317bff65d7b44c02b2032c9b2f773e4201cb
roi.kramer <roi.kramer@codefresh.io>
2021-11-12 11:51:20 +0200

    wip

--- cmd/commands/integration.go
+++ cmd/commands/integration.go
@@ -5,38 +5,33 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/codefresh-io/cli-v2/pkg/log"
+	"github.com/codefresh-io/cli-v2/pkg/util"
+	sdk "github.com/codefresh-io/go-sdk/pkg/codefresh"
+	model "github.com/codefresh-io/go-sdk/pkg/codefresh/model/app-proxy"
 	"github.com/juju/ansiterm"
 	"github.com/spf13/cobra"
+)
+
+type (
+	GitIntegrationAddOptions struct {
+		Name          string
+		Provider      model.GitProviders
+		SharingPolicy model.SharingPolicy
+	}
 )
 
 func NewIntegrationCommand() *cobra.Command {
 	var (
 		runtime string
+		client  sdk.AppProxyAPI
 	)
 
 	cmd := &cobra.Command{
-		Use:     "integration",
-		Aliases: []string{"integrations", "intg"},
-		Short:   "Manage integrations with git providers, container registries and more",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if err := cfConfig.RequireAuthentication(cmd, args); err != nil {
-				return err
-			}
-
-			if runtime != "" {
-				return nil
-			}
-
-			cur := cfConfig.GetCurrentContext()
-
-			if cur.DefaultRuntime == "" {
-				return fmt.Errorf("missing name of runtime to use")
-			}
-
-			runtime = cur.DefaultRuntime
-
-			return nil
-		},
+		Use:               "integration",
+		Aliases:           []string{"integrations", "intg"},
+		Short:             "Manage integrations with git providers, container registries and more",
+		PersistentPreRunE: getAppProxyClient(&runtime, &client),
 		Run: func(cmd *cobra.Command, args []string) {
 			cmd.HelpFunc()(cmd, args)
 			exit(1)
@@ -45,12 +40,12 @@ func NewIntegrationCommand() *cobra.Command {
 
 	cmd.PersistentFlags().StringVar(&runtime, "runtime", "", "Name of runtime to use")
 
-	cmd.AddCommand(NewGitIntegrationCommand(&runtime))
+	cmd.AddCommand(NewGitIntegrationCommand(&client))
 
 	return cmd
 }
 
-func NewGitIntegrationCommand(runtime *string) *cobra.Command {
+func NewGitIntegrationCommand(client *sdk.AppProxyAPI) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "git",
 		Short: "Manage your git integrations",
@@ -60,30 +55,30 @@ func NewGitIntegrationCommand(runtime *string) *cobra.Command {
 		},
 	}
 
-	cmd.AddCommand(NewGitIntegrationListCommand(runtime))
+	cmd.AddCommand(NewGitIntegrationListCommand(client))
+	cmd.AddCommand(NewGitIntegrationAddCommand(client))
+	cmd.AddCommand(NewGitIntegrationEditCommand(client))
+	cmd.AddCommand(NewGitIntegrationRemoveCommand(client))
+	cmd.AddCommand(NewGitIntegrationRegisterCommand(client))
+	cmd.AddCommand(NewGitIntegrationDeregisterCommand(client))
 
 	return cmd
 }
 
-func NewGitIntegrationListCommand(runtime *string) *cobra.Command {
+func NewGitIntegrationListCommand(client *sdk.AppProxyAPI) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "List your git integrations",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return RunGitIntegrationListCommand(cmd.Context(), *runtime)
+			return RunGitIntegrationListCommand(cmd.Context(), *client)
 		},
 	}
 
 	return cmd
 }
 
-func RunGitIntegrationListCommand(ctx context.Context, runtime string) error {
-	appProxy, err := cfConfig.NewClient().AppProxy(ctx, runtime)
-	if err != nil {
-		return err
-	}
-
-	integrations, err := appProxy.GitIntegrations().List(ctx)
+func RunGitIntegrationListCommand(ctx context.Context, client sdk.AppProxyAPI) error {
+	integrations, err := client.GitIntegrations().List(ctx)
 	if err != nil {
 		return err
 	}
@@ -108,4 +103,138 @@ func RunGitIntegrationListCommand(ctx context.Context, runtime string) error {
 	}
 
 	return tb.Flush()
+}
+
+func NewGitIntegrationAddCommand(client *sdk.AppProxyAPI) *cobra.Command {
+	var (
+		opts              model.AddGitIntegrationArgs
+		provider          string
+		accountAdminsOnly bool
+	)
+
+	providers := map[string]model.GitProviders{
+		"github": model.GitProvidersGithub,
+		"gitlab": model.GitProvidersGitlab,
+	}
+
+	cmd := &cobra.Command{
+		Use:   "add NAME",
+		Short: "Add a new git integration",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return fmt.Errorf("missing integration name")
+			}
+
+			opts.Name = args[0]
+
+			p, ok := providers[provider]
+			if !ok {
+				return fmt.Errorf("provider '%s' is not a valid provider name", provider)
+			}
+			opts.Provider = p
+
+			opts.SharingPolicy = model.SharingPolicyAllUsersInAccount
+			if accountAdminsOnly {
+				opts.SharingPolicy = model.SharingPolicyAccountAdmins
+			}
+
+			return RunGitIntegrationAddCommand(cmd.Context(), *client, &opts)
+		},
+	}
+
+	cmd.Flags().StringVar(&provider, "provider", "github", "One of github|gitlab")
+	cmd.Flags().StringVar(&opts.APIURL, "api-url", "", "Git provider API Url")
+	cmd.Flags().BoolVar(&accountAdminsOnly, "account-admins-only", false, "If true, this integration would only be visible to account admins")
+
+	util.Die(cobra.MarkFlagRequired(cmd.Flags(), "api-url"))
+
+	return cmd
+}
+
+func RunGitIntegrationAddCommand(ctx context.Context, client sdk.AppProxyAPI, opts *model.AddGitIntegrationArgs) error {
+	intg, err := client.GitIntegrations().Add(ctx, opts)
+	if err != nil {
+		return fmt.Errorf("failed to add git integration: %w", err)
+	}
+
+	log.G(ctx).Infof("created git integration: %s", intg.Name)
+
+	return nil
+}
+
+func NewGitIntegrationEditCommand(client *sdk.AppProxyAPI) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List your git integrations",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunGitIntegrationListCommand(cmd.Context(), *client)
+		},
+	}
+
+	return cmd
+}
+
+func NewGitIntegrationRemoveCommand(client *sdk.AppProxyAPI) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List your git integrations",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunGitIntegrationListCommand(cmd.Context(), *client)
+		},
+	}
+
+	return cmd
+}
+
+func NewGitIntegrationRegisterCommand(client *sdk.AppProxyAPI) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List your git integrations",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunGitIntegrationListCommand(cmd.Context(), *client)
+		},
+	}
+
+	return cmd
+}
+
+func NewGitIntegrationDeregisterCommand(client *sdk.AppProxyAPI) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List your git integrations",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunGitIntegrationListCommand(cmd.Context(), *client)
+		},
+	}
+
+	return cmd
+}
+
+func getAppProxyClient(runtime *string, client *sdk.AppProxyAPI) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		if err := cfConfig.RequireAuthentication(cmd, args); err != nil {
+			return err
+		}
+
+		if *runtime != "" {
+			return nil
+		}
+
+		cur := cfConfig.GetCurrentContext()
+
+		if cur.DefaultRuntime == "" {
+			return fmt.Errorf("missing name of runtime to use")
+		}
+
+		*runtime = cur.DefaultRuntime
+
+		appProxy, err := cfConfig.NewClient().AppProxy(cmd.Context(), *runtime)
+		if err != nil {
+			return err
+		}
+
+		*client = appProxy
+
+		return nil
+	}
 }
