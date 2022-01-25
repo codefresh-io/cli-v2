37452b93e9188fdfd404aef03cc2f456b8d23cd7
roi.kramer <roi.kramer@codefresh.io>
2022-01-25 14:31:30 +0200

    fix wrong command error

--- cmd/commands/git-source.go
+++ cmd/commands/git-source.go
@@ -11,21 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// Copyright 2021 The Codefresh Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package commands
 
 import (
@@ -73,7 +58,7 @@ type (
 		CreateDemoResources bool
 		Exclude             string
 		Include             string
-		IngressHost		 	string
+		IngressHost         string
 	}
 
 	GitSourceDeleteOptions struct {
@@ -109,6 +94,7 @@ func NewGitSourceCommand() *cobra.Command {
 		Use:               "git-source",
 		Short:             "Manage git-sources of Codefresh runtimes",
 		PersistentPreRunE: cfConfig.RequireAuthentication,
+		Args:              cobra.NoArgs, // Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 		Run: func(cmd *cobra.Command, args []string) {
 			cmd.HelpFunc()(cmd, args)
 			exit(1)
@@ -699,7 +685,7 @@ func createGithubExampleIngress() *netv1.Ingress {
 				ServiceName: store.Get().GithubExampleEventSourceObjectName + "-eventsource-svc",
 				ServicePort: store.Get().GithubExampleEventSourceServicePort,
 			},
-		},	})
+		}})
 }
 
 func getRepoOwnerAndNameFromRepoURL(repoURL string) (owner string, name string) {
