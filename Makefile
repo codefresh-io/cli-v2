a060039a93931ae65f91cb56e6346fd969b9d9ff
roi-codefresh <roi.kramer@codefresh.io>
2022-01-25 15:07:34 +0200

    fix wrong command error (#219)
    * fix wrong command error
    
    * bump

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=v0.0.199
+VERSION=v0.0.200
 
 OUT_DIR=dist
 YEAR?=$(shell date +"%Y")
--- cmd/commands/config.go
+++ cmd/commands/config.go
@@ -12,20 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright 2021 The Codefresh Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package commands
 
 import (
@@ -45,6 +31,7 @@ func NewConfigCommand() *cobra.Command {
 		Use:               "config",
 		Short:             "Manage Codefresh authentication contexts",
 		PersistentPreRunE: cfConfig.Load,
+		Args:              cobra.NoArgs, // Workaround for subcommand usage errors. See: https://github.com/spf13/cobra/issues/706
 		Long: util.Doc(`By default, Codefresh authentication contexts are persisted at $HOME/.cfconfig.
 You can create, delete and list authentication contexts using the following
 commands, respectively:
