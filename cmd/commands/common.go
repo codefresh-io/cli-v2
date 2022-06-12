24d9d17962bca24b6cf73316c44ddd83614a1253
Rotem Cohen <rotem.cohen@codefresh.io>
2022-06-12 19:02:44 +0300

    CR-11767 uninstalling isc when uninstalling runtime (#424)
    * isc uninstall
    
    * bump
    
    * get isc repo from`me` query
    
    * usersV2
    
    * get current
    
    * bump go-sdk
    
    * bump
    
    * bump

--- cmd/commands/common.go
+++ cmd/commands/common.go
@@ -637,3 +637,12 @@ func setIscRepo(ctx context.Context, suggestedSharedConfigRepo string) (string, 
 
 	return setIscRepoResponse, nil
 }
+
+func getIscRepo(ctx context.Context) (string, error) {
+	user, err := cfConfig.NewClient().V2().UsersV2().GetCurrent(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to get shared config repo. Error: %w", err)
+	}
+
+	return *user.ActiveAccount.SharedConfigRepo, nil
+}
