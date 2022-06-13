74bed62d362cf1398c23b14599d6cda235d582b6
rotem-cf <rotem.cohen@codefresh.io>
2022-06-13 11:53:31 +0300

    set cluster name

--- cmd/commands/cluster.go
+++ cmd/commands/cluster.go
@@ -37,6 +37,7 @@ import (
 type (
 	ClusterAddOptions struct {
 		runtimeName string
+		clusterName string
 		kubeContext string
 		kubeconfig  string
 		dryRun      bool
@@ -104,6 +105,7 @@ func newClusterAddCommand() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().StringVar(&opts.clusterName, "name", "", "Name of the cluster. If omitted, will use the context name")
 	cmd.Flags().BoolVar(&opts.dryRun, "dry-run", false, "")
 	opts.kubeFactory = kube.AddFlags(cmd.Flags())
 
@@ -136,7 +138,8 @@ func runClusterAdd(ctx context.Context, opts *ClusterAddOptions) error {
 	}
 
 	csdpToken := cfConfig.GetCurrentContext().Token
-	k := createAddClusterKustomization(ingressUrl, opts.kubeContext, server, csdpToken, *runtime.RuntimeVersion)
+	setClusterName(opts)
+	k := createAddClusterKustomization(ingressUrl, opts.clusterName, server, csdpToken, *runtime.RuntimeVersion)
 
 	manifests, err := kustutil.BuildKustomization(k)
 	if err != nil {
@@ -154,6 +157,13 @@ func runClusterAdd(ctx context.Context, opts *ClusterAddOptions) error {
 	}
 
 	return kubeutil.WaitForJob(ctx, opts.kubeFactory, "kube-system", store.Get().AddClusterJobName)
+}
+
+func setClusterName(opts *ClusterAddOptions) {
+	if opts.clusterName != "" {
+		return
+	}
+	opts.clusterName = opts.kubeContext
 }
 
 func createAddClusterKustomization(ingressUrl, contextName, server, csdpToken, version string) *kusttypes.Kustomization {
