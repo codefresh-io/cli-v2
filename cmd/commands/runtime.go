f56d47f8a734736a607ad7dc7b82473cbfc3c5d6
elad-codefresh <elad.laor@codefresh.io>
2022-06-15 11:01:19 +0300

    edit appset func

--- cmd/commands/runtime.go
+++ cmd/commands/runtime.go
@@ -781,6 +781,12 @@ func RunRuntimeInstall(ctx context.Context, opts *RuntimeInstallOptions) error {
 		return util.DecorateErrorWithDocsLink(fmt.Errorf("failed to create project: %w", err))
 	}
 
+	r, fs, err := opts.InsCloneOpts.GetRepo(ctx)
+	if err != nil {
+		return util.DecorateErrorWithDocsLink(fmt.Errorf("failed editing the application set. %w", err))
+	}
+	editAppSet(ctx, r, fs, opts.RuntimeName)
+
 	// persists codefresh-cm, this must be created before events-reporter eventsource
 	// otherwise it will not start and no events will get to the platform.
 	if !opts.FromRepo {
@@ -2347,6 +2353,36 @@ func createReporter(ctx context.Context, cloneOpts *git.CloneOptions, opts *Runt
 	pushMessage := "Created Codefresh" + titleCase.String(reporterCreateOpts.reporterName) + "Reporter"
 
 	return apu.PushWithMessage(ctx, r, pushMessage)
+}
+
+func editAppSet(ctx context.Context, r git.Repository, repofs fs.FS, rtName string) error {
+	projPath := repofs.Join(apstore.Default.ProjectsDir, rtName + ".yaml")
+	_, appset, err := getProjectInfoFromFile(repofs, projPath)
+	if err != nil {
+		return err
+	}
+
+	// in order to prevent resource-adjustment made by gke autopilot
+	appset.Spec.Template.Spec.IgnoreDifferences = append(appset.Spec.Template.Spec.IgnoreDifferences, argocdv1alpha1.ResourceIgnoreDifferences{
+			Group: "apps",
+			Kind:  "Deployment",
+			Name: "cap-app-proxy",
+			JSONPointers: []string{
+				"/spec",
+			},
+	})
+	
+	err = repofs.WriteYamls(projPath, appset)
+	if err != nil {
+		return err
+	}
+
+	err = apu.PushWithMessage(ctx, r, "edited the application-set")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func updateProject(repofs fs.FS, rt *runtime.Runtime) error {
