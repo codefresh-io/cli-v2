55651a4f38b92564005bff20b0dce84cdaec72fd
Daniel Maizel <daniel.maizel@codefresh.io>
2022-07-18 11:29:24 +0300

    bump app-proxy (#490)
    * bump app-proxy
    
    * initial commit
    
    * fix
    
    * push codegen

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -73,6 +73,7 @@ type (
 		Name       string `json:"name"`
 		Type       string `json:"type"`
 		URL        string `json:"url"`
+		SyncWave   int    `json:"syncWave"`
 		Wait       bool   `json:"wait"`
 		IsInternal bool   `json:"isInternal"`
 	}
@@ -270,7 +271,7 @@ func (a *AppDef) CreateApp(ctx context.Context, f kube.Factory, cloneOpts *git.C
 		timeout = store.Get().WaitTimeout
 	}
 
-	return apcmd.RunAppCreate(ctx, &apcmd.AppCreateOptions{
+	appCreateOpts := &apcmd.AppCreateOptions{
 		CloneOpts:     cloneOpts,
 		AppsCloneOpts: &git.CloneOptions{},
 		ProjectName:   projectName,
@@ -283,12 +284,17 @@ func (a *AppDef) CreateApp(ctx context.Context, f kube.Factory, cloneOpts *git.C
 				util.EscapeAppsetFieldName(store.Get().LabelKeyCFType):     cfType,
 				util.EscapeAppsetFieldName(store.Get().LabelKeyCFInternal): strconv.FormatBool(a.IsInternal),
 			},
+			Annotations: map[string]string{
+				util.EscapeAppsetFieldName(store.Get().AnnotationKeySyncWave): strconv.Itoa(a.SyncWave),
+			},
 			Exclude: exclude,
 			Include: include,
 		},
 		KubeFactory: f,
 		Timeout:     timeout,
-	})
+	}
+
+	return apcmd.RunAppCreate(ctx, appCreateOpts)
 }
 
 func (a *AppDef) delete(fs fs.FS) error {
--- pkg/store/store.go
+++ pkg/store/store.go
@@ -79,6 +79,7 @@ type Store struct {
 	DocsLink                            string
 	LabelKeyCFType                      string
 	LabelKeyCFInternal                  string
+	AnnotationKeySyncWave               string
 	MarketplaceGitSourceName            string
 	MarketplaceRepo                     string
 	MaxDefVersion                       *semver.Version
@@ -188,6 +189,7 @@ func init() {
 	s.DocsLink = "https://codefresh.io/csdp-docs/"
 	s.LabelKeyCFType = "codefresh.io/entity"
 	s.LabelKeyCFInternal = "codefresh.io/internal"
+	s.AnnotationKeySyncWave = "argocd.argoproj.io/sync-wave"
 	s.MaxDefVersion = semver.MustParse(maxDefVersion)
 	s.RuntimeDefURL = RuntimeDefURL
 	s.MarketplaceGitSourceName = "marketplace-git-source"
