4b0cfc3e1212406ef09c5f1b7e8f41ecbc3a7a50
Noam Gal <noam.gal@codefresh.io>
2022-10-02 09:27:24 +0300

    CR-14744 - fix skip-ingress flag (#585)
    * check SkipIngress flag when needed

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -116,7 +116,6 @@ type Store struct {
 	ArgoCD                            string
 	Silent                            bool
 	InsecureIngressHost               bool
-	BypassIngressClassCheck           bool
 	SetDefaultResources               bool
 	MinimumMemorySizeRequired         string
 	MinimumCpuRequired                string
