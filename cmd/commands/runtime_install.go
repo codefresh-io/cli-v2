14623f5fd4514fc9b2cbc1196b5f2303429a3e90
vitaliichyrka <vitalii.chyrka@codefresh.io>
2025-01-16 17:50:16 +0200

    run codegen

--- cmd/commands/runtime_install.go
+++ cmd/commands/runtime_install.go
@@ -1,3 +1,17 @@
+// Copyright 2025 The Codefresh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Copyright 2024 The Codefresh Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -168,7 +182,7 @@ func NewRuntimeInstallCommand() *cobra.Command {
 	)
 
 	cmd := &cobra.Command{
-		Use: "install [runtime_name]",
+		Use:        "install [runtime_name]",
 		Deprecated: "We have transitioned our GitOps Runtimes from CLI-based to Helm-based installation.",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			return errors.New(`We have transitioned our GitOps Runtimes from CLI-based to Helm-based installation.
