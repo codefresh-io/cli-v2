68edd5d72cb8b8a3e1e1db25c367753162931ed1
andrii-codefresh <andrii@codefresh.io>
2025-04-22 22:54:23 +0300

    log

--- cmd/commands/account.go
+++ cmd/commands/account.go
@@ -174,7 +174,7 @@ func ValidateGitOpsUsage(usage platmodel.GitOpsUsage, limits platmodel.GitOpsLim
 		default:
 			return fmt.Errorf("invalid fail condition")
 		}
-		log.G().Infof("Limit checking succeeded. subject=%s, usage=%d, limit=%d\n", subject, *usageVal, *limitVal)
+		log.G().Infof("Limit checking succeeded. condition=%s, subject=%s, usage=%d, limit=%d", failCondition, subject, *usageVal, *limitVal)
 		return nil
 	}
 
